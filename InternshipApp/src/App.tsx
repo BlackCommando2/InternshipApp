d317c874a4364ee8ecca680b3278b507c913aef2
BlackCommando2 <keshavsoni2002@gmail.com>
2023-08-19 19:35:31 +0530

    feat: added dark theme

--- InternshipApp/src/App.tsx
+++ InternshipApp/src/App.tsx
@@ -2,15 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FormPage from './components/FormPage';
 import SecondPage from './components/SecondPage';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<FormPage />} />
-        <Route path="/second" element={<SecondPage />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<FormPage />} />
+          <Route path="/second" element={<SecondPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
